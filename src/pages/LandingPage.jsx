7af56cfe10796b80ce14852c874248759649f991
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-07-29 22:43:37 +0200

    landing done

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,6 +6,10 @@ import Login from "../components/Login";
 import Signup from "../components/Signup";
 import Paper from "@material-ui/core/Paper";
 import logo from "../assets/images/logo_dark_transparent.png";
+import Animation from '../components/Animation';
+import landing from '../json/landing.json';
+import './landing.css';
+import {Grid} from '@material-ui/core';
 
 function LandingPage(props) {
   const [signup, setSignUp] = useState(false);
@@ -18,36 +22,66 @@ function LandingPage(props) {
 
   return (
     <div className="landingPageWrapper">
-      <Container>
-        <div className="landingLogoDescWrapper" align="center">
-          <img src={logo} width="300px" alt={logo} />
-          <Typography
-            variant="h6"
-            gutterBottom
-            align="center"
-            color="secondary"
-          >
-            Connect with people over topics that excite you
-          </Typography>
-        </div>
-        <div className="landingBtnsWrapper" align="center">
-          <Button
-            onClick={handleLoginPopUp}
-            type="submit"
-            variant="outlined"
-            color="primary"
-          >
-            Login
-          </Button>
-          <Button
-            onClick={handleSignUpPopUp}
-            type="submit"
-            variant="outlined"
-            color="primary"
-          >
-            Sign Up
-          </Button>
-        </div>
+      <Container className="cont-landing">
+
+
+
+      <Grid   container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    alignContent="center"
+                    spacing={4}
+                >
+
+                   
+        <Grid item xs={12} sm={6} >
+          <div className="logo-btns" >
+            <div className="landingLogoDescWrapper" align="center">
+                <img id="logo-landing" src={logo} width="200px" alt="logo" />
+                <h2 id="desc">
+                  Connect with people over topics that excite you
+                </h2>
+              </div>
+              <div className="landingBtnsWrapper" align="center">
+                <Button
+                  onClick={handleLoginPopUp}
+                  type="submit"
+                  id="login-btn"
+                >
+                  Login
+                </Button>
+                <Button
+                  onClick={handleSignUpPopUp}
+                  type="submit"
+                  id="signup-btn"
+                >
+                  Sign Up
+                </Button>
+              </div>
+
+
+            </div>    
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <div className="animation-wrapper">
+            <Animation width={700} height={700} animation={landing} />
+          </div>
+          <br />
+          <div className="madeBy">
+            <p>Made by 
+            <a target="_blank" href="https://www.linkedin.com/in/kalandemwamba/" > Kalande</a>, 
+            <a target="_blank" href="https://www.linkedin.com/in/liliia-khrystiuk/"> Lily</a> & 
+            <a target="_blank" href="https://www.linkedin.com/in/bartspangenberg/"> Bart</a></p>
+          </div>
+        </Grid>
+      </Grid>
+
+
+          
+
+
       </Container>
       {signup && (
         <div className="popupOpacity">
@@ -59,6 +93,8 @@ function LandingPage(props) {
           <Login onLoginPopUp={handleLoginPopUp} onLogin={props.onLogin} />
         </div>
       )}
+
+
     </div>
   );
 }
